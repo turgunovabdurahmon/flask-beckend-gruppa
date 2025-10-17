8552bb9798450de32d6fcef591669213dbac7e87
turgunov abdurahmon <abdurahmonturgunov212@gmail.com>
2025-10-17 16:44:36 +0500

    new update

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask("__name__")
 def index():
     return "Salom backend dasturchi"
 
+@app.route("/home")
+def home():
+    return "Home page"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
